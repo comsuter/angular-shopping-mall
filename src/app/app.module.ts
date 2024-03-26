6898549efd9c51d55fe37296191e2c0bbb778f1c
sue <todayilearn@naver.com>
2024-03-26 13:42:34 +0900

    기본 컴포넌트 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { JoinComponent } from './join/join.component';
+import { MainComponent } from './main/main.component';
+import { ErrorComponent } from './error/error.component';
+import { ProductListComponent } from './main/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    JoinComponent,
+    MainComponent,
+    ErrorComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main/product-list/product-list.component.ts
@@ -0,0 +1,13 @@
+import { Component , inject } from '@angular/core';
+import { ProductModel } from '../../../model/product.model';
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+}
+
